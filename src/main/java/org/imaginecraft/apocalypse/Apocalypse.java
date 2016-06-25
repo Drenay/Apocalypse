7cf9890901274c8e9c0e75fefc00a3671ebfcd6f
Drenay <drenay@hotmail.com>
2016-06-25 19:03:50 +0200

    Skeleton plugin project

---
+++ src/main/java/org/imaginecraft/apocalypse/Apocalypse.java
@@ -0,0 +1,17 @@
+package org.imaginecraft.apocalypse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Apocalypse extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
